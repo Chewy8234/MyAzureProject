21ab44546934c73f5866555960f8b7b25b85156d
sebastian campos <srcampos8611@eagle.fgcu.edu>
2021-10-05 19:43:44 -0400

    updated Todoitem.cs

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -1,7 +1,8 @@
 public class TodoItem
 {
-    public string Title { get; set; }       <!-- returning the title = Todo, setter takes the parameter of Todo-->   
-    public bool IsDone { get; set; }        <!-- returning true or false due to bool, setter takes in the parameter-->
-                                           
-                                             
+    // returning the title = Todo, setter takes the parameter of Todo
+    public string Title { get; set; 
+    public bool IsDone { get; set; }
+    //returning true or false due to bool, setter takes in the parameter             
+
 }
