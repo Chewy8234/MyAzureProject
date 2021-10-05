6df24a4f6222f4e010339152133f6e60cae0857f
sebastian campos <srcampos8611@eagle.fgcu.edu>
2021-10-05 19:38:51 -0400

    Added Todo.razor, updated NavMenu.razor and added TodoItem.cs

---
+++ Client/TodoItem.cs
@@ -0,0 +1,7 @@
+public class TodoItem
+{
+    public string Title { get; set; }       <!-- returning the title = Todo, setter takes the parameter of Todo-->   
+    public bool IsDone { get; set; }        <!-- returning true or false due to bool, setter takes in the parameter-->
+                                           
+                                             
+}
