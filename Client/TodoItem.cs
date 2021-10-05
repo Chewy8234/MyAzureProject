74f85ff674a2f3dee35ee4119014efeeb68683e6
sebastian campos <srcampos8611@eagle.fgcu.edu>
2021-10-05 19:48:27 -0400

    Updated Todo.razor and TodoItem.cs

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -1,8 +1,8 @@
 public class TodoItem
 {
-    // returning the title = Todo, setter takes the parameter of Todo
+                                                                        // returning the title = Todo, setter takes the parameter of Todo
     public string Title { get; set; 
-    public bool IsDone { get; set; }
-    //returning true or false due to bool, setter takes in the parameter             
+    public bool IsDone { get; set; }                                     //returning true or false due to bool, setter takes in the parameter          
+
 
 }
