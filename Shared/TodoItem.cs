03e0cf2284c7ff4ef043e06f3e410f9ae4288fec
sebastian campos <srcampos8611@eagle.fgcu.edu>
2021-10-05 20:32:39 -0400

    Updated Todo.cs

--- Shared/TodoItem.cs
+++ Shared/TodoItem.cs
@@ -1,7 +1,7 @@
 public class TodoItem
 {
                                                                         // returning the title = Todo, setter takes the parameter of Todo
-    public string Title { get; set; 
+    public string Title { get; set; }
     public bool IsDone { get; set; }                                     //returning true or false due to bool, setter takes in the parameter          
 
 
