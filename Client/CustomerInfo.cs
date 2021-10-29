cd4b52c1c0e0da0f45246e010364d17672232783
sebastian campos <srcampos8611@eagle.fgcu.edu>
2021-10-29 15:51:42 -0400

    updated Customer, Math and NavMenu.razor

---
+++ Client/CustomerInfo.cs
@@ -0,0 +1,13 @@
+
+
+//public class Customer:ComponentBase
+//{
+//    protected string firstName;
+//    protected string lastName;
+
+//}
+
+//public class Employee:Customer
+//{
+//    protected string employeeName;
+//}
